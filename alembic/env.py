67edc38cce0362c0111f31809fb3997f0f59f2f6
tumb <sirsavil@yandex.ru>
2025-04-20 13:08:33 +0300

    chore(Alembic): env косметические правки

--- alembic/env.py
+++ alembic/env.py
@@ -26,7 +26,6 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-print("DATABASE_URL:", DATABASE_URL)
 config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 def run_migrations_offline() -> None:
