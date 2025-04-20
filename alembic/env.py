cd9c5c95f8f70eb91d3e76d2662fd8401e6326b4
tumb <sirsavil@yandex.ru>
2025-04-20 10:47:09 +0300

    chore(alembic): стартовые конфиги

--- alembic/env.py
+++ alembic/env.py
@@ -18,15 +18,16 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models.db_models import Base
+from app.core.config import DATABASE_URL
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
+print("DATABASE_URL:", DATABASE_URL)
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
